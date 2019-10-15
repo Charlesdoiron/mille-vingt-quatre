a4e435bea96af228fe27be2db4a532dca3ec69fe
Charlesdoiron <doironcharles@gmail.com>
2019-10-15 22:18:36 +0200

    Update contact.js

--- src/templates/contact.js
+++ src/templates/contact.js
@@ -3,7 +3,7 @@ import { Link, graphql } from "gatsby"
 
 import { H1 } from "../components/typos"
 
-import Layout from "../components/Layout"
+
 class ContactContentfulTemplate extends React.Component {
   render() {
     return <p>mldsldslqmsdq</p>
