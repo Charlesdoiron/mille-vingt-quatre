0f74d3e1cae052bb26d45120442d71ebce875b6f
charlesdoiron <doironcharles@gmail.com>
2019-10-15 20:14:50 +0200

    add Layout

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Menu from "./Menu"
+
+const Layout = props => {
+  return (
+    <div className="layout">
+      <Menu />
+      {props.children}
+    </div>
+  )
+}
+export default Layout
