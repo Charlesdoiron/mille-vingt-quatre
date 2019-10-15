b94134a33bb788ab40e8686e72d1ea422fb72564
Charlesdoiron <doironcharles@gmail.com>
2019-10-15 22:19:15 +0200

    Update project.js

--- src/templates/project.js
+++ src/templates/project.js
@@ -3,14 +3,14 @@ import { Link, graphql } from "gatsby"
 
 import { H1 } from "../components/typos"
 
-import Layout from "../components/Layout"
+import layout from "../components/layout"
 class project extends React.Component {
   render() {
     const project = this.props.data.contentfulProject
     const { previous, next } = this.props.pageContext
 
     return (
-      <Layout>
+      <layout>
         <div className="project__container">
           <H1>{project.projectTitle}</H1>
 
@@ -46,7 +46,7 @@ class project extends React.Component {
             </li>
           </ul>
         </div>
-      </Layout>
+      </layout>
     )
   }
 }
