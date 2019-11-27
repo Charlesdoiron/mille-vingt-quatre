16fc893ad41942a0b69650d19c4232df82918ec9
charlesdoiron <doironcharles@gmail.com>
2019-11-27 10:28:18 +0100

    add case sensitive plugin

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,5 @@
 const path = require(`path`)
-
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin")
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   const { createRedirect } = actions
@@ -119,4 +119,16 @@ exports.createPages = ({ graphql, actions }) => {
   })
 
   return Promise.all([loadBlogPosts, loadPages, loadProjects])
+}
+
+exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    plugins: [new CaseSensitivePathsPlugin({ debug: true })],
+  })
 }
