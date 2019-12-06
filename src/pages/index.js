d345f1ad4067020044ba34fe2be5d63404b9abcb
charlesdoiron <doironcharles@gmail.com>
2019-12-06 14:02:17 +0100

    heroky preset

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { graphql } from "gatsby"
 import styled from "styled-components"
 import { Layout } from "../components/newLayout"
@@ -47,6 +47,12 @@ const Page = props => {
   console.log("POSTS", posts)
   console.log("CURRENT", currentPage)
   console.log("MODULES", modules)
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" })
+  }, [])
+
+  console.log("WINDOW", window)
 
   const renderModulesOnPages = modules => {
     if (currentPage === "blog") {
