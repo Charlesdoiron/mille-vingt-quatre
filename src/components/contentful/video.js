06a43bd75f92a898b2d510ff37de47c243c6fbdc
charlesdoiron <doironcharles@gmail.com>
2019-12-13 12:29:24 +0100

    improve image mobile

--- src/components/contentful/video.js
+++ src/components/contentful/video.js
@@ -9,22 +9,24 @@ export const Video = props => {
   const isCover = props.video.display && props.video.display[0] === "Is-Cover"
 
   return (
-    <ImgBlur className={classNames("video__container", { isCover: isCover })}>
-      <iframe
-        // src={`https://player.vimeo.com/video/${video_id}?title=0&byline=0&portrait=0&controls=0&autoplay=1&loop=1&autopause=1&autopause=0`}
-        src={
-          isGif || isCover
-            ? `https://player.vimeo.com/video/${video_id}?title=0&byline=0&portrait=0&controls=0&autoplay=1&loop=1&autopause=1&autopause=0&background=1`
-            : `https://player.vimeo.com/video/${video_id}?title=0&byline=0&portrait=0&controls=1`
-        }
-        frameBorder="0"
-        allow="autoplay; fullscreen"
-        autoplay
-        loop
-        allowFullScreen
-        title="video"
-      ></iframe>
-      {isCover && <Gradient />}
-    </ImgBlur>
+    <div data-aos={isCover !== undefined ? "" : `fade-up`}>
+      <ImgBlur className={classNames("video__container", { isCover: isCover })}>
+        <iframe
+          // src={`https://player.vimeo.com/video/${video_id}?title=0&byline=0&portrait=0&controls=0&autoplay=1&loop=1&autopause=1&autopause=0`}
+          src={
+            isGif || isCover
+              ? `https://player.vimeo.com/video/${video_id}?title=0&byline=0&portrait=0&controls=0&autoplay=1&loop=1&autopause=1&autopause=0&background=1`
+              : `https://player.vimeo.com/video/${video_id}?title=0&byline=0&portrait=0&controls=1`
+          }
+          frameBorder="0"
+          allow="autoplay; fullscreen"
+          autoplay
+          loop
+          allowFullScreen
+          title="video"
+        ></iframe>
+        {isCover && <Gradient />}
+      </ImgBlur>
+    </div>
   )
 }
