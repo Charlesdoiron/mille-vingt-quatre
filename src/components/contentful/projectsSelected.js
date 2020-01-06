0d124ff43c50e29231a5b213a8a6f5a15c97aa10
charlesdoiron <doironcharles@gmail.com>
2020-01-06 17:36:14 +0100

    fix

--- src/components/contentful/projectsSelected.js
+++ src/components/contentful/projectsSelected.js
@@ -1,10 +1,31 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { ProjectsSelectedList } from "./projectsSelectedList"
 import { ProjectsSelectedSliderMobile } from "./projectsSelectedSliderMobile"
 
-import { ShowIfDeviceIs as showIfDeviceIs } from "../../utils/mediaqueries"
 export const ProjectsSelected = props => {
-  if (showIfDeviceIs("mobile")) {
+  const [device, setDevice] = useState("")
+  const getWidth = () => {
+    if (window.innerWidth > 992) {
+      setDevice("desktop")
+    } else if (678 < window.innerWidth && window.innerWidth > 992) {
+      setDevice("tablet")
+    } else {
+      setDevice("mobile")
+    }
+  }
+  useEffect(() => {
+    getWidth()
+  }, [])
+  useEffect(() => {
+    if (typeof window !== undefined) {
+      window.addEventListener("resize", () => getWidth(), {
+        passive: true,
+      })
+      return () => window.removeEventListener("resize", () => getWidth())
+    }
+  }, [device])
+
+  if (device === "mobile") {
     return (
       <ProjectsSelectedSliderMobile projectSelected={props.projectSelected} />
     )
