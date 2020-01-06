e7551220b882e3ea536fbb154cf96e8866b3e714
charlesdoiron <doironcharles@gmail.com>
2020-01-06 17:19:28 +0100

    fix

--- src/utils/Mediaqueries.js
+++ src/utils/Mediaqueries.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react"
 
 export function ShowIfDeviceIs(arg) {
   const [device, setDevice] = useState("")
-  const ShowIfDeviceIs = () => {
+  const getWidth = () => {
     if (window.innerWidth > 992) {
       setDevice("desktop")
     } else if (678 < window.innerWidth && window.innerWidth > 992) {
@@ -12,14 +12,14 @@ export function ShowIfDeviceIs(arg) {
     }
   }
   useEffect(() => {
-    ShowIfDeviceIs()
+    getWidth()
   }, [])
   useEffect(() => {
     if (typeof window !== undefined) {
-      window.addEventListener("resize", () => ShowIfDeviceIs(), {
+      window.addEventListener("resize", () => getWidth(), {
         passive: true,
       })
-      return () => window.removeEventListener("resize", () => ShowIfDeviceIs())
+      return () => window.removeEventListener("resize", () => getWidth())
     }
   }, [device])
 
